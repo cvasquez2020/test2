8db98e6d6f4dafdaabfd265f90213156cf7ec9ac
Alex Vasquez <caalevas@gmail.com>
2020-01-03 09:26:15 -0500

    First commit

---
+++ test.java
@@ -0,0 +1,18 @@
+
+ */
+
+public class HiddenWord {
+    import java.util.Scanner;
+    public static void main(String[] args) {
+        HiddenWord puzzle = new HiddenWord("HARPS");
+    }
+
+    public HiddenWord(String hiddenword) {
+
+
+
+    }
+    public getHint(String guess) {
+        String guess = in.next();
+    }
+}
